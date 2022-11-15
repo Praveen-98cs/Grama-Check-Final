a2e23c35bfc795f796e016a8273c5b034022abe4
Praveen Bhawantha <1praveenbhawantha@gmail.com>
2022-11-15 13:12:48 +0530

    new

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 const Index = () => (
   <AuthProvider
       config={ {
-          signInRedirectURL: "https://gramacheck-gr0uyg8a1-praveen-98cs.vercel.app/",
-          signOutRedirectURL: "https://gramacheck-gr0uyg8a1-praveen-98cs.vercel.app/",
+          signInRedirectURL: "https://gramacheck.vercel.app",
+          signOutRedirectURL: "https://gramacheck.vercel.app",
           clientID: "Pcwt8fDyLvxgUrmIaPg3Wv6JKBEa",
           baseUrl: "https://api.asgardeo.io/t/rdxorg",
           scope: [ "openid","profile" ]
